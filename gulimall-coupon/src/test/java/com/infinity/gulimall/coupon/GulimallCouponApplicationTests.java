7c8b000ce16d0de484094ab9b39bce764951d2e8
Chap1inwang <1148818926@qq.com>
2022-12-12 20:57:25 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/com/infinity/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinity.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
